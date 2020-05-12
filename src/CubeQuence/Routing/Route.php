3000a8d2395a5778cfa008cd9f7cc62f9ae91a37
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-05-12 13:31:06 +0200

    add more route methods

--- src/CubeQuence/Routing/Route.php
+++ src/CubeQuence/Routing/Route.php
@@ -11,11 +11,67 @@ class Route
      *
      * @param string $location
      * @param string $controller
+     * @param array $middleware optional
      * 
      * @return void
      */
-    public static function get($location, $controller)
+    public static function get($location, $controller, $middleware = [])
     {
-        self::$router->get($location, $controller);
+        self::$router->get($location, $controller, $middleware);
+    }
+
+    /**
+     * Define POST route
+     *
+     * @param string $location
+     * @param string $controller
+     * @param array $middleware optional
+     * 
+     * @return void
+     */
+    public static function post($location, $controller, $middleware = [])
+    {
+        self::$router->post($location, $controller, $middleware);
+    }
+
+    /**
+     * Define PATCH route
+     *
+     * @param string $location
+     * @param string $controller
+     * 
+     * @return void
+     */
+    public static function patch($location, $controller, $middleware = [])
+    {
+        self::$router->patch($location, $controller, $middleware);
+    }
+
+    /**
+     * Define DELETE route
+     *
+     * @param string $location
+     * @param string $controller
+     * @param array $middleware optional
+     * 
+     * @return void
+     */
+    public static function delete($location, $controller, $middleware = [])
+    {
+        self::$router->delete($location, $controller, $middleware);
+    }
+
+    /**
+     * Define ANY route
+     *
+     * @param string $location
+     * @param string $controller
+     * @param array $middleware optional
+     * 
+     * @return void
+     */
+    public static function any($location, $controller, $middleware = [])
+    {
+        self::$router->any($location, $controller, $middleware);
     }
 }
