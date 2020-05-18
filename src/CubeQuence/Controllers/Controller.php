d8f3c6b936f3a851561ca8ac219e6c82b276ab54
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-05-18 14:53:59 +0200

    update json response

--- src/CubeQuence/Controllers/Controller.php
+++ src/CubeQuence/Controllers/Controller.php
@@ -63,38 +63,18 @@ class Controller
     /**
      * Shorthand JSON response function
      * 
-     * @param array $data
+     * @param string $message
+     * @param array $data optional
      * @param integer $code optional
      * 
-     * @return Json
+     * @return JsonResponse
      */
-    protected function respondJson($data = [], $code = 200)
+    protected function respondJson($message, $data = [], $code = 200)
     {
         return new Json([
-            'success' => true,
+            'success' => $code === 200,
+            'message' => $message,
             'data' => $data
-        ], $code);
-    }
-
-    /**
-     * Shorthand JSON error response function
-     *
-     * @param string $title
-     * @param string $detail optional
-     * @param integer $code optional
-     * 
-     * @return Json
-     */
-    protected function respondJsonError($title, $detail = null, $code = 400)
-    {
-        return new Json([
-            'success' => false,
-            'errors' => [
-                'status' => $code,
-                'title' => $title,
-                'detail' => $detail
-
-            ]
         ], $code);
     }
 }
