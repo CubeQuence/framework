77791c9991cc966fdb2aa5b7e7b5c9cc09c45661
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-05-20 12:25:14 +0200

    finish ratelimit middleware

--- src/CubeQuence/Middleware/RateLimit.php
+++ src/CubeQuence/Middleware/RateLimit.php
@@ -17,8 +17,8 @@ class RateLimit extends Middleware
      */
     public function __construct()
     {
-        $this->max_requests = Config::get('ratelimit.max_requests') ?: 60;
-        $this->reset_time = Config::get('ratelimit.reset_time') ?: 60;
+        $this->max_requests = (int) Config::get('ratelimit.max_requests') ?: 60;
+        $this->reset_time = (int) Config::get('ratelimit.reset_time') ?: 60;
     }
 
     /**
@@ -31,18 +31,16 @@ class RateLimit extends Middleware
      */
     public function handle($request, $next)
     {
-        $signature = $this->resolveRequestSignature($request);
-        $ratelimit_exceeded = true;
+        $fingerprint = $this->fingerprintRequest($request);
+        $validated_request = $this->validateRequest($fingerprint);
 
-        if ($ratelimit_exceeded) {
-            return $this->buildResponse($signature);
+        if (!$validated_request['valid']) {
+            return $this->buildResponse($validated_request);
         }
 
-        $headers = $this->genHeaders(
-            $this->calculateRemainingRequests($signature)
-        );
-        $response = $next($request);
+        $headers = $this->genHeaders($validated_request);
 
+        $response = $next($request);
         foreach ($headers as $key => $value) {
             $response = $response->withHeader($key, $value);
         }
@@ -51,13 +49,13 @@ class RateLimit extends Middleware
     }
 
     /**
-     * Resolve request signature.
+     * Resolve request fingerprint.
      *
      * @param object $request
      *
      * @return string
      */
-    protected function resolveRequestSignature($request)
+    protected function fingerprintRequest($request)
     {
         return sha1(
             $request->getMethod() .
@@ -69,16 +67,13 @@ class RateLimit extends Middleware
     /**
      * Create a 'too many requests' response.
      *
-     * @param  string $signature
+     * @param array $validated_request
      *
      * @return Json
      */
-    protected function buildResponse($signature)
+    protected function buildResponse($validated_request)
     {
-        $headers = $this->genHeaders(
-            $this->calculateRemainingRequests($signature),
-            // TODO: $this->limiter->avalibleIn($signature)
-        );
+        $headers = $this->genHeaders($validated_request,);
 
         return new Json([
             'success' => false,
@@ -99,15 +94,15 @@ class RateLimit extends Middleware
      *
      * @return array
      */
-    protected function genHeaders($remaining_requests, $retry_after = null)
+    protected function genHeaders($validated_request)
     {
         $headers = [
             'X-RateLimit-Limit' => $this->max_requests,
-            'X-RateLimit-Remaining' => $remaining_requests
+            'X-RateLimit-Remaining' => $validated_request['remaining_requests']
         ];
 
-        if (!is_null($retry_after)) {
-            $headers['Retry-After'] = $retry_after;
+        if (!$validated_request['valid']) {
+            $headers['X-RateLimit-Reset'] = $validated_request['reset_time'];
         }
 
         return $headers;
@@ -116,49 +111,69 @@ class RateLimit extends Middleware
     /**
      * Calculate the number of remaining requests.
      *
-     * @param string $signature
+     * @param string $fingerprint
      *
-     * @return int
+     * @return array
      */
-    protected function calculateRemainingRequests($signature)
+    protected function validateRequest($fingerprint)
     {
         $request = DB::get('cq_ratelimit', ['counter', 'reset_time'], [
-            'signature' => $signature
+            'fingerprint' => $fingerprint
         ]);
 
         if (!$request) {
-            $request = DB::create('cq_ratelimti', [
-                'signature' => $signature,
+            $request = DB::create('cq_ratelimit', [
+                'fingerprint' => $fingerprint,
                 'counter' => 1,
                 'reset_time' => time() + $this->reset_time
             ]);
         }
 
+        $counter = $this->incrementRequest(
+            $fingerprint,
+            $request['reset_time'],
+            $request['counter']
+        );
+
+        return [
+            'valid' => ($this->max_requests - $counter) > 0,
+            'remaining_requests' => $this->max_requests - $counter,
+            'reset_time' => $request['reset_time']
+        ];
+    }
+
+    /**
+     * Add one or reset request counter
+     *
+     * @param string $fingerprint
+     * @param int $reset_time
+     * @param int $current_counter
+     * 
+     * @return int
+     */
+    protected function incrementRequest($fingerprint, $reset_time, $current_counter)
+    {
         // reset time in past
-        if (time() > $request['reset_time']) {
-            DB::update('cq_ratelimti', [
+        if (time() > $reset_time) {
+            DB::update('cq_ratelimit', [
                 'counter' => 1,
                 'reset_time' => time() + $this->reset_time
             ], [
-                'signature' => $signature
+                'fingerprint' => $fingerprint
             ]);
 
-            $counter = 1;
+            return 1;
         }
 
         // reset time in future
-        if (time() > $request['reset_time']) {
-            DB::update('cq_ratelimti', [
+        if ($current_counter < $this->max_requests) {
+            DB::update('cq_ratelimit', [
                 'counter[+]' => 1
             ], [
-                'signature' => $signature
+                'fingerprint' => $fingerprint
             ]);
-
-            $counter = $request['counter']++;
         }
 
-        $remaining_requests = $this->max_requests - $counter;
-
-        return $remaining_requests;
+        return $current_counter++;
     }
 }
