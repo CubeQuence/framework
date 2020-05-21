9d51c5ab1c4bd9956152c1e38d7ff8c9fb829c1d
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-05-22 01:27:55 +0200

    Update JSON.php

--- src/CubeQuence/Middleware/JSON.php
+++ src/CubeQuence/Middleware/JSON.php
@@ -9,7 +9,7 @@ use CQ\Middleware\Middleware;
 class JSON extends Middleware
 {
     /**
-     * If requests contains JSON interpret it
+     * If POST,PUT,PATCH,DELETE requests contains JSON interpret it
      * Also validate that the provided JSON is valid.
      *
      * @param object $request
@@ -19,31 +19,33 @@ class JSON extends Middleware
      */
     public function handle($request, $next)
     {
-        if (!Request::isJson($request)) {
-            return new JsonResponse([
-                'success' => false,
-                'errors' => [
-                    'status' => 415,
-                    'title' => 'invalid_content_type',
-                    'detail' => "Content-Type should be 'application/json'"
-                ]
-            ], 415);
+        if (in_array($request->getMethod(), ['POST', 'PUT', 'PATCH', 'DELETE'])) {
+            if (!Request::isJson($request)) {
+                return new JsonResponse([
+                    'success' => false,
+                    'errors' => [
+                        'status' => 415,
+                        'title' => 'invalid_content_type',
+                        'detail' => "Content-Type should be 'application/json'"
+                    ]
+                ], 415);
+            }
+
+            $data = json_decode($request->getBody()->getContents());
+
+            if ((JSON_ERROR_NONE !== json_last_error())) {
+                return new JsonResponse([
+                    'success' => false,
+                    'errors' => [
+                        'status' => 415,
+                        'title' => 'invalid_json',
+                        'detail' => 'Problems parsing provided JSON'
+                    ]
+                ], 415);
+            }
+
+            $request->data = $data;
         }
-
-        $data = json_decode($request->getBody()->getContents());
-
-        if ((JSON_ERROR_NONE !== json_last_error())) {
-            return new JsonResponse([
-                'success' => false,
-                'errors' => [
-                    'status' => 415,
-                    'title' => 'invalid_json',
-                    'detail' => 'Problems parsing provided JSON'
-                ]
-            ], 415);
-        }
-
-        $request->data = $data;
 
         return $next($request);
     }
