3522e21abfc975ca58283bf6cb0b5b9fdd95fd31
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-05-23 20:48:30 +0200

    add renderFromText  method

--- src/CubeQuence/Response/Twig.php
+++ src/CubeQuence/Response/Twig.php
@@ -3,8 +3,8 @@
 namespace CQ\Response;
 
 use Twig\Environment;
+use Twig\Loader\ArrayLoader;
 use Twig\Loader\FilesystemLoader;
-use Twig\TwigFunction;
 
 class Twig
 {
@@ -35,5 +35,21 @@ class Twig
     public function get()
     {
         return $this->twig;
+    }
+
+    /**
+     * Render template in string form
+     *
+     * @param string $template
+     * @param array $parameters
+     * 
+     * @return string
+     */
+    public static function renderFromText($template, $parameters = [])
+    {
+        $loader = new ArrayLoader(['base.html' => $template]);
+        $twig = new Environment($loader);
+
+        return $twig->render('base.html', $parameters);
     }
 }
