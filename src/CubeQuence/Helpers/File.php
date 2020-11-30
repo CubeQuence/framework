08c29374331fbf8beb42af7bab169a1f1f9de8e9
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-11-30 14:43:20 +0100

    add file exists check, and add copy function

--- src/CubeQuence/Helpers/File.php
+++ src/CubeQuence/Helpers/File.php
@@ -25,6 +25,10 @@ class File
      */
     private function open($path, $mode = 'a+')
     {
+        if (!self::exists($path)) {
+            throw new Exception('File does not exist');
+        }
+
         $handle = fopen($path, $mode);
 
         if (!$handle) {
@@ -79,7 +83,7 @@ class File
     public function append($data)
     {
         if (!fwrite($this->file, $data)) {
-            throw new Exception('Cannot write to file');
+            throw new Exception('Cannot append to file');
         }
     }
 
@@ -120,6 +124,25 @@ class File
     }
 
     /**
+     * Copy file
+     *
+     * @param string $original_file
+     * @param string $new_file
+     *
+     * @return void
+     */
+    public static function copy($original_file, $new_file)
+    {
+        if (!self::exists($original_file)) {
+            throw new Exception('File does not exist');
+        }
+
+        if (!copy($original_file, $new_file)) {
+            throw new Exception('Cannot copy file');
+        }
+    }
+
+    /**
      * Get mime type
      *
      * @param string $file_path
@@ -129,7 +152,7 @@ class File
     public static function getMimeType($file_path)
     {
         if (!self::exists($file_path)) {
-            throw new Exception('Cannot open file');
+            throw new Exception('File does not exist');
         }
 
         $finfo = finfo_open(FILEINFO_MIME_TYPE);
