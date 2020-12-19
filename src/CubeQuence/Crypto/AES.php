273ba5ed5430be92741061065fa65d7178be89c5
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-12-19 18:23:05 +0100

    add auto key function

--- src/CubeQuence/Crypto/AES.php
+++ src/CubeQuence/Crypto/AES.php
@@ -2,21 +2,43 @@
 
 namespace CQ\Crypto;
 
+use CQ\Config\Config;
 use phpseclib3\Crypt\AES as AESLib;
 use phpseclib3\Crypt\Random;
+use Exception;
 
 class AES
 {
     /**
-     * Encrypt string
+     * Get encryption key
      *
-     * @param string $key
-     * @param string $string
+     * @param string $key optional
      *
      * @return string
      */
-    public static function encrypt($key, $string)
+    private static function getKey($key = null)
     {
+        $key = $key ?: Config::get('app.key');
+
+        if (!$key) {
+            throw new Exception('No key found!');
+        }
+
+        return $key;
+    }
+
+    /**
+     * Encrypt string
+     *
+     * @param string $string
+     * @param string $key optional
+     *
+     * @return string
+     */
+    public static function encrypt($string, $key = null)
+    {
+        $key = self::getKey($key);
+
         $cipher = new AESLib('ctr');
         $iv = Random::string(16);
 
@@ -31,13 +53,15 @@ class AES
     /**
      * Decrypt string
      *
-     * @param string $key
      * @param string $enc_string
+     * @param string $key optional
      *
      * @return string
      */
-    public static function decrypt($key, $enc_string)
+    public static function decrypt($enc_string, $key = null)
     {
+        $key = self::getKey($key);
+
         list($decoded_enc_string, $iv) = explode('|', $enc_string);
 
         $cipher = new AESLib('ctr');
