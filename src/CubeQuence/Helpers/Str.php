a2585837a22ae6ab667f56bfe1784d88669c17b6
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-12-20 23:13:29 +0100

    add zero width helper to str

--- src/CubeQuence/Helpers/Str.php
+++ src/CubeQuence/Helpers/Str.php
@@ -66,4 +66,107 @@ class Str
 
         return $string;
     }
+
+    /**
+     * Convert string to binary
+     *
+     * @param string $string
+     *
+     * @return string
+     */
+    public static function toBinary($string)
+    {
+        $characters = str_split($string);
+
+        $binary = [];
+
+        foreach ($characters as $character) {
+            $data = unpack('H*', $character);
+            $binary[] = base_convert($data[1], 16, 2);
+        }
+
+        return implode(' ', $binary);
+    }
+
+    /**
+    * Convert binary to string
+    *
+    * @param string $binary
+    *
+    * @return string
+    */
+    public static function binaryToString($binary)
+    {
+        $binaries = explode(' ', $binary);
+
+        $string = null;
+
+        foreach ($binaries as $binary) {
+            $string .= pack('H*', dechex(bindec($binary)));
+        }
+
+        return $string;
+    }
+
+    /**
+     * Insert zero width secret in string
+     *
+     * @param string $string
+     * @param string $secret
+     *
+     * @return string
+     */
+    public static function insertZeroWidth($string, $secret)
+    {
+        $binary = self::toBinary($secret);
+        $characters = explode('', $binary);
+
+        $output = array_map(function ($character) {
+            if ($character === 1) {
+                return '​';
+            }
+
+            if ($character === 0) {
+                return '‌';
+            }
+
+            return '‍';
+        }, $characters);
+
+        return $string . implode('﻿', $output);
+    }
+
+    /**
+     * Insert zero width secret in string
+     *
+     * @param string $string
+     * @param string $secret
+     *
+     * @return string
+     */
+    public static function extractZeroWidth($string)
+    {
+        $clean_string = str_replace("&#8203;", '', $string);
+        $clean_string = str_replace("&#8204;", '', $clean_string);
+        $clean_string = str_replace("&#8205;", '', $clean_string);
+        $clean_string = str_replace("&#65279;", '', $clean_string);
+
+        $input = str_replace($clean_string, '', $string);
+
+        $output = array_map(function ($character) {
+            if ($character === '​') {
+                return 1;
+            }
+
+            if ($character === '‌') {
+                return 0;
+            }
+
+            return ' ';
+        }, $input);
+
+        $binary = implode('', $output);
+
+        return self::binaryToString($binary);
+    }
 }
